fc66d95f3e8052353fe2cac02c29ca9fc4b98e22
Eugene Havrylov <eugeneh@dev6.com>
2019-11-12 14:35:51 -0500

    About page created

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import React from "react"
 import logo from "../images/navlogo.png"
 import headerStyles from "./header.module.scss"
-import { Navbar, NavbarBrand } from "react-bootstrap"
+import { Navbar, Nav } from "react-bootstrap"
 import BasketIcon from "./basket-icon"
 import { useStateValue } from "../state/state"
 
@@ -13,13 +13,21 @@ const Header = ({ siteTitle }) => {
   return (
     <header className={headerStyles.container}>
       <Navbar variant="light" sticky={"top"} className={`py-0 ${headerStyles.shadowNav}`}>
-        <NavbarBrand>
+        <Navbar.Brand>
           <Link to="/" className="navbar-brand">
             <img src={logo} alt={siteTitle} />
           </Link>
-        </NavbarBrand>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="justify-content-end d-flex" id="basic-navbar-nav">
+          <Nav.Link
+            className={headerStyles.navLink}
+            as={Link}
+            to={"/about"}
+            activeStyle={{ backgroundColor: "#0B6689" }}
+          >
+            About
+          </Nav.Link>
           <div className={headerStyles.vr} />
           <div className={headerStyles.cartBtn}>
             <BasketIcon color={"#10A2DC"} size={2} />
