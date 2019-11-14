ec626347096e79ef6f618f9b15fdb7d20c2a9079
Eugene Havrylov <eugeneh@dev6.com>
2019-11-13 19:55:05 -0500

    product template page fetches data by graphql query

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -1,13 +1,31 @@
 import React from "react"
 import ProductItem from "../components/product-item"
 import Layout from "../hoc/layout"
+import { graphql } from "gatsby"
 
-const ProductTemplate = props => {
-  console.log("Function: ProductTemplate, props: ", props)
+const ProductTemplate = ({ data }) => {
   return (
     <Layout>
-      <ProductItem productItem={props.pageContext} />
+      <ProductItem productItem={data.dataJson} />
     </Layout>
   )
 }
+
+export const query = graphql`
+  query productQuery($id: String!) {
+    dataJson(id: { eq: $id }) {
+      id
+      items {
+        id
+        image
+        price
+        qty
+        size
+      }
+      gender
+      description
+      name
+    }
+  }
+`
 export default ProductTemplate
