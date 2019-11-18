34d0ea8cda8f8520162f968974497b54d4899ace
Eugene Havrylov <eugeneh@dev6.com>
2019-11-18 15:34:14 -0500

    test

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,7 @@
 import CMS from "netlify-cms-app"
 import uploadcare from "netlify-cms-media-library-uploadcare"
 import cloudinary from "netlify-cms-media-library-cloudinary"
-import AboutPagePreview from "./about-page"
+import AboutPagePreview from "./aboutPagePreview"
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
