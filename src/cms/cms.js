66e2d69ffb830753d031be08e4fc8a462f91f1dc
Eugene Havrylov <eugeneh@dev6.com>
2019-11-18 15:15:33 -0500

    about page preview with a template

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,9 @@
 import CMS from "netlify-cms-app"
 import uploadcare from "netlify-cms-media-library-uploadcare"
 import cloudinary from "netlify-cms-media-library-cloudinary"
+import AboutPagePreview from "./about-page"
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
+
+CMS.registerPreviewTemplate("about", AboutPagePreview)
