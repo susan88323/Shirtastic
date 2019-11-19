a8a515b297622954db1bf55521d97ea9a1683c75
Eugene Havrylov <eugeneh@dev6.com>
2019-11-19 14:12:04 -0500

    .

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,28 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-transformer-remark`,
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-relative-images",
+            options: {
+              name: "uploads",
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-transformer-json`,
     `gatsby-plugin-sharp`,
     {
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,10 @@
 const path = require("path")
 const { createFilePath } = require("gatsby-source-filesystem")
+const { fmImagesToRelative } = require("gatsby-remark-relative-images")
 
-exports.createPages = ({ graphql, actions, reporter }) => {
+exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
-  return graphql(`
+  const result = await graphql(`
     {
       allDataJson(limit: 1000) {
         edges {
@@ -15,65 +16,106 @@ exports.createPages = ({ graphql, actions, reporter }) => {
       }
     }
   `)
-    .then(result => {
-      if (result.errors) {
-        throw result.errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    throw result.errors
+  }
+  const products = result.data.allDataJson.edges
+  const productsPerPage = 3
+  const numPages = Math.ceil(products.length / productsPerPage)
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/` : `/${i + 1}`,
+      component: path.resolve("./src/templates/index.js"),
+      context: {
+        limit: productsPerPage,
+        skip: i * productsPerPage,
+        numPages,
+        currentPage: i + 1,
+        gender: "//",
+      },
+    })
+  })
+  const productsMen = result.data.allDataJson.edges.filter(edge => edge.node.gender === "M")
+  const numPagesMen = Math.ceil(productsMen.length / productsPerPage)
+  Array.from({ length: numPagesMen }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/men` : `/men/${i + 1}`,
+      component: path.resolve("./src/templates/index.js"),
+      context: {
+        limit: productsPerPage,
+        skip: i * productsPerPage,
+        numPages: numPagesMen,
+        currentPage: i + 1,
+        gender: "/M/",
+      },
+    })
+  })
+  const productsWomen = result.data.allDataJson.edges.filter(edge => edge.node.gender === "W")
+  const numPagesWomen = Math.ceil(productsWomen.length / productsPerPage)
+  Array.from({ length: numPagesMen }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/women` : `/women/${i + 1}`,
+      component: path.resolve("./src/templates/index.js"),
+      context: {
+        limit: productsPerPage,
+        skip: i * productsPerPage,
+        numPages: numPagesWomen,
+        currentPage: i + 1,
+        gender: "/W/",
+      },
+    })
+  })
+  result.data.allDataJson.edges.forEach(({ node }) => {
+    createPage({
+      path: `/product/${node.id}`,
+      component: path.resolve("./src/templates/product-template.js"),
+      context: { id: node.id },
+    })
+  })
+
+  const allPages = await graphql(`
+    query AllPages {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              templateKey
+            }
+          }
+        }
       }
-      const products = result.data.allDataJson.edges
-      const productsPerPage = 3
-      const numPages = Math.ceil(products.length / productsPerPage)
-      Array.from({ length: numPages }).forEach((_, i) => {
-        createPage({
-          path: i === 0 ? `/` : `/${i + 1}`,
-          component: path.resolve("./src/templates/index.js"),
-          context: {
-            limit: productsPerPage,
-            skip: i * productsPerPage,
-            numPages,
-            currentPage: i + 1,
-            gender: "//",
-          },
-        })
+    }
+  `)
+  if (allPages.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+  }
+
+  allPages.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    if (String(node.frontmatter.templateKey) !== "product-template") {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`src/templates/${String(node.frontmatter.templateKey)}.js`),
+        context: { id: node.id },
       })
-      const productsMen = result.data.allDataJson.edges.filter(edge => edge.node.gender === "M")
-      const numPagesMen = Math.ceil(productsMen.length / productsPerPage)
-      Array.from({ length: numPagesMen }).forEach((_, i) => {
-        createPage({
-          path: i === 0 ? `/men` : `/men/${i + 1}`,
-          component: path.resolve("./src/templates/index.js"),
-          context: {
-            limit: productsPerPage,
-            skip: i * productsPerPage,
-            numPages: numPagesMen,
-            currentPage: i + 1,
-            gender: "/M/",
-          },
-        })
-      })
-      const productsWomen = result.data.allDataJson.edges.filter(edge => edge.node.gender === "W")
-      const numPagesWomen = Math.ceil(productsWomen.length / productsPerPage)
-      Array.from({ length: numPagesMen }).forEach((_, i) => {
-        createPage({
-          path: i === 0 ? `/women` : `/women/${i + 1}`,
-          component: path.resolve("./src/templates/index.js"),
-          context: {
-            limit: productsPerPage,
-            skip: i * productsPerPage,
-            numPages: numPagesWomen,
-            currentPage: i + 1,
-            gender: "/W/",
-          },
-        })
-      })
-      result.data.allDataJson.edges.forEach(({ node }) => {
-        createPage({
-          path: `/product/${node.id}`,
-          component: path.resolve("./src/templates/product-template.js"),
-          context: { id: node.id },
-        })
-      })
+    }
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  fmImagesToRelative(node) // convert image paths for gatsby images
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
     })
-    .catch(error => {
-      reporter.panicOnBuild(`Error while running GraphQL query.`)
-    })
+  }
 }
